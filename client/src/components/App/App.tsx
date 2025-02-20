0130b1ab4377c268f068b7207b150063be9609e1
Ilya Kochetkov <ilya-ko@bk.ru>
2025-02-21 00:13:51 +0400

    added proxy to frontend server

--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -1,10 +1,23 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import axios from "axios";
 
 export default function App() {
+  const [answer, setAnswer] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("api/admin/ping")
+      .then((response) => setAnswer(response.data))
+      .catch((error) => setAnswer(error.message));
+  }, []);
+
   return (
     <>
       <h1>Todo app</h1>
-      <input type="text" placeholder="Enter your todo" />
+      <input className="input" type="text" placeholder="Enter your todo" />
+      <button className="button">Add</button>
+      <div>Server answer: {answer}</div>
     </>
   );
 }
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,17 @@
-import { defineConfig } from 'vite';
-import plugin from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import plugin from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [plugin()],
-    server: {
-        port: 61261,
-    }
-})
+  plugins: [plugin()],
+  server: {
+    port: 61261,
+    proxy: {
+      "/api": {
+        target: "http://localhost:5166",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
